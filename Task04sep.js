7c6508ca9b8b45d87752d4e5c8e7612def9d29ce
surendar2808 <130973831+surendar2808@users.noreply.github.com>
2024-09-05 13:30:33 +0530

    Create Task04sep.js

---
+++ Task04sep.js
@@ -0,0 +1,74 @@
+/*using for write a program to print even and odd number using for loop number 1 to 20
+write a program  to print all week days and week ends using switch case
+write a program  to print 10 20 30 40 50 60 70 80 90 100*/
+ 
+// program to print even and odd number between 1 to 20
+
+/*let a=20;
+console.log("even numbers : ");
+for(let i=1;i<=a;i++)
+{
+    
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+    
+}
+console.log("ODD NUMBERS :")
+for(let i=1;i<=a;i++)
+    {
+        
+        if(i%2==0)
+        {
+            continue;
+        }
+        else{
+            console.log(i);
+        }
+    }
+
+
+    //  2. All weekdays and weekends using switch case
+
+    let day=5;
+    switch(day)
+    {
+        case 1:
+            console.log("this is monday");
+            break;
+        case 2:
+            console.log("This is tuesday");
+            break;
+        case 3:
+            console.log("This wednesday");
+            break;
+        case 4:
+            console.log("This is Thursday");
+            break;
+        case 5:
+            console.log("This is Friday");
+            break;
+        case 6:
+            console.log("This is Saturday");
+            break;
+        case 7:
+            console.log("This is sunday");
+            break;
+        default:
+            console.log("enter the value between 1 to 7")
+    }       
+
+
+        // 3. write a program  to print 10 20 30 40 50 60 70 80 90 100
+
+            let n=100;
+            let i=10;
+            while(i<=n)
+            {
+                console.log(i);
+                i=i+10;
+                
+            }
+                */
+    
